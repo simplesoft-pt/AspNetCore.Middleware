bf677bc00b1d82247c2755851b3bfe9ba9a0df99
João Simões <nogravity00@outlook.com>
2018-03-17 00:50:50 +0000

    Metadata middleware options

---
+++ src/SimpleSoft.AspNetCore.Middleware.Metadata/MetadataOptions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SimpleSoft.AspNetCore.Middleware.Metadata
+{
+    /// <summary>
+    /// Metadata middleware options
+    /// </summary>
+    public class MetadataOptions
+    {
+        /// <summary>
+        /// Path for which the middleware responds
+        /// </summary>
+        public string Path { get; set; } = "api/_meta";
+
+        /// <summary>
+        /// Indent the metadata JSON response?
+        /// </summary>
+        public bool IndentJson { get; set; } = false;
+
+        /// <summary>
+        /// The API name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The API environment
+        /// </summary>
+        public string Environment { get; set; }
+
+        /// <summary>
+        /// The API startup date and time
+        /// </summary>
+        public DateTimeOffset StartedOn { get; set; } = DateTimeOffset.Now;
+
+        /// <summary>
+        /// The API version
+        /// </summary>
+        public MetadataVersionOptions Version { get; set; } = new MetadataVersionOptions();
+    }
+}
---
+++ src/SimpleSoft.AspNetCore.Middleware.Metadata/MetadataVersionOptions.cs
@@ -0,0 +1,33 @@
+﻿namespace SimpleSoft.AspNetCore.Middleware.Metadata
+{
+    /// <summary>
+    /// Version information
+    /// </summary>
+    public class MetadataVersionOptions
+    {
+        /// <summary>
+        /// Major version
+        /// </summary>
+        public uint Major { get; set; }
+
+        /// <summary>
+        /// Minor version
+        /// </summary>
+        public uint Minor { get; set; }
+
+        /// <summary>
+        /// Patch version
+        /// </summary>
+        public uint Patch { get; set; }
+
+        /// <summary>
+        /// Revision version
+        /// </summary>
+        public uint Revision { get; set; }
+
+        /// <summary>
+        /// Version alias. Defaults to '0.0.0'
+        /// </summary>
+        public string Alias { get; set; } = "0.0.0";
+    }
+}
