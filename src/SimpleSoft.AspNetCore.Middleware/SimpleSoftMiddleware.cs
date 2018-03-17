1dd25a53098ee9bd414676b7d4e6ed98a98c923a
João Simões <nogravity00@outlook.com>
2018-03-17 00:15:53 +0000

    Base middleware class

---
+++ src/SimpleSoft.AspNetCore.Middleware/SimpleSoftMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace SimpleSoft.AspNetCore.Middleware
+{
+    /// <summary>
+    /// Base class for all SimpleSoft middleware
+    /// </summary>
+    public class SimpleSoftMiddleware
+    {
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="next">The request delegate</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public SimpleSoftMiddleware(RequestDelegate next)
+        {
+            Next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        /// <summary>
+        /// The request delegate
+        /// </summary>
+        protected RequestDelegate Next { get; }
+    }
+}
