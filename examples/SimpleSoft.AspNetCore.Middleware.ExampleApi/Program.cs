32d771868670c70908ef65dc3b01415158c0aed7
João Simões <nogravity00@outlook.com>
2018-04-11 22:59:25 +0100

    Log debug and scopes

--- examples/SimpleSoft.AspNetCore.Middleware.ExampleApi/Program.cs
+++ examples/SimpleSoft.AspNetCore.Middleware.ExampleApi/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace SimpleSoft.AspNetCore.Middleware.ExampleApi
 {
@@ -13,6 +14,14 @@ namespace SimpleSoft.AspNetCore.Middleware.ExampleApi
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureLogging((ctx, builder) =>
+                {
+                    builder
+                        .ClearProviders()
+                        .AddDebug()
+                        .AddConsole(options => { options.IncludeScopes = true; })
+                        .SetMinimumLevel(LogLevel.Debug);
+                })
                 .Build();
     }
 }
