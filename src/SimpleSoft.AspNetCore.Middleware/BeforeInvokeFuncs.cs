d01785d3bf10e61334dfe1b8c526ca7114eebbb0
João Simões <nogravity00@outlook.com>
2018-04-16 01:37:53 +0100

    Helpers for checking roles

--- src/SimpleSoft.AspNetCore.Middleware/BeforeInvokeFuncs.cs
+++ src/SimpleSoft.AspNetCore.Middleware/BeforeInvokeFuncs.cs
@@ -23,6 +23,7 @@
 #endregion
 
 using System;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -49,6 +50,56 @@ namespace SimpleSoft.AspNetCore.Middleware
 
             return Task.CompletedTask;
         };
+
+        /// <summary>
+        /// Checks if the user has any of the given roles, writing HTTP 403
+        /// or HTTP 401 if not authenticated.
+        /// </summary>
+        /// <param name="role">Role to match</param>
+        /// <param name="roles">Other opcional roles to match</param>
+        /// <returns>The function to be used</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static Func<HttpContext, Task> HasAnyRole(string role, params string[] roles)
+        {
+            if (role == null) throw new ArgumentNullException(nameof(role));
+            if (roles == null) throw new ArgumentNullException(nameof(roles));
+
+            return ctx =>
+            {
+                int statusCode;
+                string content;
+                if (ctx.User.Identity.IsAuthenticated)
+                {
+                    if (ctx.User.IsInRole(role) || roles.Length > 0 && roles.Any(r => ctx.User.IsInRole(r)))
+                        return Task.CompletedTask;
+
+                    statusCode = 403;
+                    content = "Forbidden";
+                }
+                else
+                {
+                    statusCode = 401;
+                    content = "Unauthorized";
+                }
+
+                ctx.Response.StatusCode = statusCode;
+                return ctx.Response.WriteAsync(content);
+            };
+        }
+
+        /// <summary>
+        /// Checks if the request is local, otherwise if the user has any of the given roles, writing HTTP 403 
+        /// or HTTP 401 if not authenticated.
+        /// </summary>
+        /// <param name="role">Role to match</param>
+        /// <param name="roles">Other opcional roles to match</param>
+        /// <returns>The function to be used</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static Func<HttpContext, Task> IsLocalOrHasAnyRole(string role, params string[] roles)
+        {
+            var hasAnyRole = HasAnyRole(role, roles);
+            return ctx => ctx.Request.IsLocal() ? Task.CompletedTask : hasAnyRole(ctx);
+        }
 
         private static bool IsLocal(this HttpRequest req)
         {
