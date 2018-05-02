c8463ceff58da7902c016e25122512454603f69d
João Simões <nogravity00@outlook.com>
2018-05-02 17:44:58 +0100

    Release 1.1.0

--- tools/assemblymetadata.ps1
+++ tools/assemblymetadata.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = "Stop"
 
 $assemblyVersion = "1.0.0" 
-$assemblyFileVersion = "1.0.0.18106"
-$assemblyInformationalVersion = "1.0.0"
+$assemblyFileVersion = "1.0.0.18122"
+$assemblyInformationalVersion = "1.1.0"
 
 Write-Host "Updating project versions..."
 
